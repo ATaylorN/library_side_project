134c37c0934ab004be2aaa9b3028571cb40543c5
ATaylorN <amandanagies@yahoo.com>
2023-10-24 19:32:03 -0400

    adding test data

--- database/librarydatabase.sql
+++ database/librarydatabase.sql
@@ -6,16 +6,16 @@ CREATE TABLE book(
 	author varchar(120) NOT NULL,
 	summary varchar (400) NOT NULL,
 	price decimal,
-	onWishlist boolean, 
-	collection_id int,
+	onWishlist boolean,
 	hasRead boolean NOT NULL DEFAULT false, 
 	hasPurchased boolean NOT NULL DEFAULT false,
 	collection_name varchar(120),
-	genre_name varchar(50) NOT NULL);
+	genre_name varchar(50) NOT NULL
+);
 
-INSERT INTO book(title, author, summary, hasRead, hasPurchased, genre_name)
-VALUES ('The Count of Monte Cristo', 'Alexandre Dumas','test description', true, true, 'Classic Literature'),
-('Hamlet', 'William Shakespeare', 'test description', true, true, 'Classic Literature');
+INSERT INTO book(title, author, summary, price, onwishlist, hasread, hasPurchased, collection_name, genre_name)
+VALUES ('The Count of Monte Cristo', 'Alexandre Dumas','Thrown in prison for a crime he has not commited, Edmond Dantes is confined to the grim fotress of If. There he learns of a great hoard of treasure hidden on the Isle of Monte Cristo and he becomes determined not only to escape, but also to unearth the treasure and use it to plot the destruction of the three men responsible for his incarceration.', 0.00, false, true, true, 'Classics', 'Classic Literature'),
+('Hamlet', 'William Shakespeare', 'Hamlet is Shakespeares most popular, and most puzzling, play. It follows the form of a revenge tragedy, in which the hero, Hamlet, seeks vengeance against his fathers murderer, his uncle Claudius, now the king of Denmark.', 0.00, false, true, true, 'Classics', 'Classic Literature');
 
-COMMIT TRANSACTION;
+-- COMMIT TRANSACTION;
 --SELECT * FROM book;
