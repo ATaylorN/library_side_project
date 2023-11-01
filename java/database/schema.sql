66ad6de595c20cd8ccd8dadf4df2207e3bb71b1b
ATaylorN <amandanagies@yahoo.com>
2023-11-01 19:36:30 -0400

    test configuration setup

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, book;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,19 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE book(
+	book_id serial,
+	title varchar(120) NOT NULL,
+	author varchar(120) NOT NULL,
+	summary varchar (400) NOT NULL,
+	price decimal,
+	onWishlist boolean,
+	hasRead boolean NOT NULL DEFAULT false, 
+	hasPurchased boolean NOT NULL DEFAULT false,
+	collection_name varchar(120),
+	genre_name varchar(50) NOT NULL
 );
 
 COMMIT TRANSACTION;
--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -4,4 +4,8 @@ INSERT INTO users (username,password_hash,role) VALUES ('user1','user1','ROLE_US
 INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
 
+INSERT INTO book (book_id, title, author, summary, price, onwishlist, hasread, haspurchased, collection_name, genre_name) VALUES (1, 'Book1', 'Author1', 'test summary1', 0.50, true, true, true, 'collection1', 'genre1');
+INSERT INTO book (book_id, title, author, summary, price, onwishlist, hasread, haspurchased, collection_name, genre_name) VALUES (2, 'Book2', 'Author2', 'test summary2', 12.75, false, true, false, 'collection1', 'genre1');
+INSERT INTO book (book_id, title, author, summary, price, onwishlist, hasread, haspurchased, collection_name, genre_name) VALUES (3, 'Book3', 'Author3', 'test summary3', 17.99, true, false, false, 'collection2', 'genre2');
+
 COMMIT TRANSACTION;
